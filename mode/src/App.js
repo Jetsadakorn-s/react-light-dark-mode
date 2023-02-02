aa53f46c330a06699ded4ff61fec63b16ccf5a87
Jetsadakorn <my1471@hotmail.com>
2023-02-02 17:09:44 +0700

    Add function change theme
    background-color
    text

--- mode/src/App.js
+++ mode/src/App.js
@@ -1,13 +1,19 @@
 import Title from './component/Title'
 import Content from './component/Content'
 import './App.css'
+import { createContext, useState } from 'react'
+
+export const ThemeContext = createContext()
 
 function App() {
+  const [theme,setTheme]=useState("light")
   return(
-    <div>
-      <Title/>
-      <Content/>
-    </div>
+    <ThemeContext.Provider value={{theme,setTheme}}>
+      <div>
+        <Title/>
+        <Content/>
+      </div>
+    </ThemeContext.Provider>
   )
 }
 
--- mode/src/component/Content.js
+++ mode/src/component/Content.js
@@ -1,13 +1,17 @@
 import light from '../image/light.svg'
+// eslint-disable-next-line
 import dark from '../image/dark.svg'
+import { useContext } from 'react'
+import { ThemeContext } from '../App'
 
 
 const Content=()=>{
+    const {theme}= useContext(ThemeContext)
     return(
-        <main>
+        <main className={theme==="dark"?"dark":"light"}>
             <div>
-                <h1>Jetsadkorn Official</h1>
-                <p>DarkMode Workshop</p>
+                <h1>Jetsadakorn Official</h1>
+                <p className={theme==="dark" ? "dark" : "light"}>DarkMode Workshop</p>
             </div>
             <img src={light} alt="Logo"/>
         </main>
